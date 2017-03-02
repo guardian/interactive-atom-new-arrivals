931039d4ba27beb1038e20b55eb8e74258fef313
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2017-03-02 11:55:21 +0000

    lunch day tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -11,4 +11,22 @@ xr.get('https://interactive.guim.co.uk/docsdata-test/1gdOsFLW7sdot9brXUCN4Uk1tBH
 
     // inject that rendered html into the empty div we declared in main.html
     document.querySelector(".interactive-blocks").innerHTML = html;
+
+    var toggleButtons = document.querySelectorAll('.toggle-text');
+    for (var i = 0; toggleButtons.length>i; i++) {
+        toggleButtons[i].onclick = function() {
+            toggleText(this.parentElement);
+        }
+    }
+
 });
+
+
+function toggleText(block) {
+    var blockButton = block.querySelector('.more');
+    if (blockButton.getAttribute('class').indexOf('is-open')>=0) {
+        blockButton.setAttribute('class', 'more');
+    } else {
+        blockButton.setAttribute('class', 'more is-open');
+    }
+}
